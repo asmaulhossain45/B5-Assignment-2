07bb7638b474f2e19c4a19ce162b158c5ca56ebf
asmaulhosen45 <asmaulhosen45@gmail.com>
2025-05-26 12:25:35 +0600

    Added WHERE clause answer

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,163 +1,32 @@
--- Active: 1747488226880@@127.0.0.1@5432@conservation_db
-CREATE DATABASE conservation_db;
-
-CREATE TABLE rangers (
-    ranger_id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    region VARCHAR(50) NOT NULL
-)
-
-INSERT INTO
-    rangers (name, region)
-VALUES (
-        'Alice Green',
-        'Northern Hills '
-    ),
-    ('Bob White', 'River Delta'),
-    (
-        'Carol King',
-        'Mountain Range'
-    );
-
-select * from rangers;
-
-CREATE TABLE species (
-    species_id SERIAL PRIMARY KEY,
-    common_name VARCHAR(50) NOT NULL,
-    scientific_name VARCHAR(50) NOT NULL,
-    discovery_date DATE NOT NULL,
-    conservation_status VARCHAR(25) NOT NULL
-);
-
-INSERT INTO
-    species (
-        common_name,
-        scientific_name,
-        discovery_date,
-        conservation_status
-    )
-VALUES (
-        'Snow Leopard',
-        'Panthera uncia',
-        '1775-01-01',
-        'Endangered'
-    ),
-    (
-        'Bengal Tiger',
-        'Panthera tigris',
-        '1758-01-01',
-        'Endangered'
-    ),
-    (
-        'Red Panda',
-        'Ailurus fulgens',
-        '1825-01-01',
-        'Vulnerable'
-    ),
-    (
-        'Asiatic Elephant',
-        'Elephas maximus indicus',
-        '1758-01-01',
-        'Endangered'
-    );
-
-SELECT * FROM species;
-
-CREATE TABLE sightings (
-    sighting_id SERIAL PRIMARY KEY,
-    ranger_id INTEGER NOT NULL REFERENCES rangers (ranger_id),
-    species_id INTEGER NOT NULL REFERENCES species (species_id),
-    location VARCHAR(100) NOT NULL,
-    sighting_time TIMESTAMP NOT NULL,
-    notes TEXT
-);
-
-INSERT INTO
-    sightings (
-        ranger_id,
-        species_id,
-        location,
-        sighting_time,
-        notes
-    )
-VALUES (
-        1,
-        1,
-        'Peak Ridge',
-        '2024-05-10 07:45:00',
-        'Camera trap image captured'
-    ),
-    (
-        2,
-        2,
-        'Bankwood Area',
-        '2024-05-12 16:20:00',
-        'Juvenile seen'
-    ),
-    (
-        3,
-        3,
-        'Bamboo Grove East',
-        '2024-05-15 09:10:00',
-        'Feeding observed'
-    ),
-    (
-        2,
-        1,
-        'Snowfall Pass',
-        '2024-05-18 18:30:00',
-        NULL
-    );
-
-SELECT * FROM sightings;
-
 -- Problem 1
-INSERT INTO
-    rangers (name, region)
+INSERT INTO rangers (name, region)
 VALUES ('Derek Fox', 'Coastal Plains');
 
-SELECT * FROM rangers;
-
 -- Problem 2
-SELECT COUNT(DISTINCT species_id) AS unique_species_count
-FROM sightings;
+SELECT COUNT(DISTINCT species_id) AS unique_species_count FROM sightings;
 
 -- Problem 3
 SELECT * FROM sightings WHERE location LIKE '%Pass%';
 
 -- Problem 4
-SELECT name, COUNT(sighting_id) AS total_sightings
-FROM rangers
+SELECT name, COUNT(sighting_id) AS total_sightings FROM rangers
     JOIN sightings ON rangers.ranger_id = sightings.ranger_id
-GROUP BY
-    name
-ORDER BY name ASC;
+    GROUP BY name ORDER BY name ASC;
 
 -- Problem 5
-SELECT sp.common_name
-FROM species AS sp
+SELECT sp.common_name FROM species AS sp
     LEFT JOIN sightings AS si ON sp.species_id = si.species_id
-WHERE
-    si.species_id IS NULL;
+    WHERE si.species_id IS NULL;
 
 -- Problem 6
-SELECT sp.common_name, si.sighting_time, ra.name
-FROM
-    sightings si
+SELECT sp.common_name, si.sighting_time, ra.name FROM sightings si
     JOIN species sp ON si.species_id = sp.species_id
     JOIN rangers ra ON si.ranger_id = ra.ranger_id
-ORDER BY si.sighting_time DESC
-LIMIT 2;
+    ORDER BY si.sighting_time DESC LIMIT 2;
 
 -- -- Problem 7
-UPDATE species
-SET
-    conservation_status = 'Historic'
-WHERE
-    EXTRACT(
-        YEAR
-        FROM discovery_date
-    ) < 1800;
+UPDATE species SET conservation_status = 'Historic'
+    WHERE EXTRACT(YEAR FROM discovery_date) < 1800;
 
 -- Problem 8
 SELECT sighting_id,
@@ -169,6 +38,6 @@ SELECT sighting_id,
 
 -- Problem 9
 DELETE FROM rangers
-WHERE ranger_id NOT IN (
+    WHERE ranger_id NOT IN (
     SELECT DISTINCT ranger_id FROM sightings
-);
+    );
